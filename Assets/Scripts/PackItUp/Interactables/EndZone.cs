1e1f31dbdfa0724b26edeebf7adecbc08a3e1508
Dennis Federico <dennisfederico@gmail.com>
2024-06-15 11:55:59 +0200

    Merged GameManager and Endzone branches and wired them

--- Assets/Scripts/PackItUp/Interactables/EndZone.cs
+++ Assets/Scripts/PackItUp/Interactables/EndZone.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -8,13 +10,23 @@ namespace PackItUp.Interactables
     [RequireComponent(typeof(Light))]
     public class EndZone : MonoBehaviour
     {
-        public delegate void EventHandler();
-        public event EventHandler OnPlayerEnteredZone;
+        //Since we have a procedural generated environment all EndZones listen to the same event to activate the visual cue
+        private static event EventHandler<bool> ActivateVisualCue;
+        
+        //All EndZones notify to the same event that the player has entered the zone (thus static)
+        public static event EventHandler<GameObject> OnPlayerEnteredZone;
+        public static event EventHandler<GameObject> OnPlayerExitZone;
+        
         [SerializeField] private Vector2 size = new Vector2(5, 2);
         private SpriteRenderer _spr;
         private BoxCollider2D _bc;
         private Light2D _light;
 
+        public static void ActivateCue(bool activate)
+        {
+            ActivateVisualCue?.Invoke(null, activate);
+        }
+        
         private void Awake()
         {
             _spr = GetComponent<SpriteRenderer>();
@@ -30,42 +42,45 @@ namespace PackItUp.Interactables
 
         private void OnEnable()
         {
+            ActivateVisualCue += OnActivateVisualCue;
             // TODO: Activate the End Zone with the Game State Manager
             // GameStateManager.OnLevelWinCondition += ActivateZone
 
             // OR call public method ActivateZone from GSM (see below)
         }
 
-        /*private void OnDisable()
+        private void OnDisable()
         {
+            ActivateVisualCue -= OnActivateVisualCue;
             // TODO: Unsubscribe to Game State Manager Event
             // GameStateManager.MandatoryItemsCollected -= ActivateZone
 
             // OR call public method ActivateZone from GSM (see below)
-        }*/
+        }
 
-        public void ActivateZone()
+        public void OnActivateVisualCue(object sender, bool activate)
         {
             // Enable light to indicate that the player can exit the level
-            _light.enabled = true;
+            _light.enabled = activate;
         }
-
-        public void DeactivateZone()
-        {
-            _light.enabled = false;
-        }
-
+        
         private void OnTriggerEnter2D(Collider2D collision)
         {
             // Get the tag of the collision's GameObject to check that it's a player
             if (collision.gameObject.CompareTag("Player"))
             {
-                Debug.Log("End Zone Triggered");
+                Debug.Log("End Zone Enter");
+                OnPlayerEnteredZone?.Invoke(this, collision.gameObject);
+            }
+        }
 
-                // Checking for subscribers
-                if (OnPlayerEnteredZone != null) {
-                    OnPlayerEnteredZone();
-                }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            // Get the tag of the collision's GameObject to check that it's a player
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("End Zone Exit");
+                OnPlayerExitZone?.Invoke(this, collision.gameObject);
             }
         }
     }
--- Assets/Scripts/PackItUp/MockSystems/MockLevel.cs
+++ Assets/Scripts/PackItUp/MockSystems/MockLevel.cs
@@ -1,11 +1,24 @@
-using System;
 using System.Collections.Generic;
+using PackItUp.Interactables;
+using PackItUp.Managers;
 using UnityEngine;
 
-public class MockLevel : MonoBehaviour
-{
-    private MockEndZone _endZone;
-    private List<object> _items;
+namespace PackItUp.MockSystems {
+    
+    /// <summary>
+    /// This mock level implementation will fetch all EndZones added to the scene.
+    /// </summary>
+    public class MockLevel : MonoBehaviour, ILevel
+    {
+        [SerializeField] private EndZone[] _endZones;
+        private List<object> _items;
 
-    public MockEndZone GetEndZone() => _endZone;
+        private void Awake() {
+            _endZones = FindObjectsByType<EndZone>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+        }
+
+        public EndZone[] GetEndZones() {
+            return _endZones;
+        }
+    }
 }
