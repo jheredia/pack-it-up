f2043b6315fcbd278ae8873c0ba406058815ff47
Dennis Federico <dennisfederico@gmail.com>
2024-06-18 23:34:11 +0200

    Convincing Movement Controller

--- Assets/Scripts/PackItUp/Controllers/TopDownCharacterController.cs
+++ Assets/Scripts/PackItUp/Controllers/TopDownCharacterController.cs
@@ -9,19 +9,21 @@ namespace PackItUp.Controllers {
     [RequireComponent(typeof(Rigidbody2D))]
     public class TopDownCharacterController : MonoBehaviour {
         
-        private const float TimeToReachTopSpeed = 0.1f;
         [SerializeField] private SOPlayerConfig playerConfig;
         [SerializeField] private SOMovementConfig movementConfig;
         private Vector2 _moveDirection;
+        private Vector2 _ghostForward = Vector2.up;
+        private Vector2 _moveForce;
         private Rigidbody2D _rb;
         private CharacterControllerBindings _playerControls;
         private InputAction _moveAction;
         private InputAction _dashAction;
         private bool _isDashing;
-        private Vector2 _dashDirection;
         private float _timeDashing;
         private float _timeSinceLastDash;
-
+        
+        [SerializeField] private bool showDebug;
+        
         private bool _isActive = true;
         
         public float NormalizedTimeUntilDash => Math.Clamp(_timeSinceLastDash / movementConfig.dashCooldown, 0, 1);
@@ -47,44 +49,51 @@ namespace PackItUp.Controllers {
         }
 
         private void FixedUpdate() {
+            
             if (!_isActive) return;
+            
             _timeSinceLastDash += Time.fixedDeltaTime;
             
             if (_isDashing) {
                 PerformDash();
             } else if (_moveDirection.magnitude > 0) {
-                Accelerate();
+                ApplyAccelerationMovement();
             } else {
                 Decelerate();
             }
+            
+            // Clamp the velocity and force to the top speed and acceleration (when not dashing)
+            if (!_isDashing) {
+                _rb.velocity = Vector2.ClampMagnitude(_rb.velocity, movementConfig.topSpeed);
+                _rb.totalForce = Vector2.ClampMagnitude(_rb.totalForce, movementConfig.acceleration);
+                _moveForce = Vector2.ClampMagnitude(_moveForce, movementConfig.acceleration);
+            }
+            
+            if (!showDebug) return;
+            Debug.DrawRay(transform.position, _rb.velocity.normalized *5, Color.red);
+            Debug.DrawRay(transform.position, _ghostForward.normalized *5, Color.green);
+            Debug.DrawRay(transform.position, _rb.totalForce.normalized *5, Color.blue);
+            Debug.DrawRay(transform.position, _moveDirection.normalized *5, Color.magenta);
         }
         
         private void Decelerate() {
-            // Decelerate based on momentum
-            var decelerationTime = (1 - movementConfig.momentum) * Time.fixedDeltaTime; // At 0 momentum, factor is 1 (instant stop). At 1 momentum, factor is 0 (no deceleration).
-            _rb.velocity = Vector2.Lerp(_rb.velocity, Vector2.zero, decelerationTime);
-            // Vector2.SmoothDamp(_rb.velocity, Vector2.zero, ref _rb.velocity, TimeToReachTopSpeed);
+            _moveForce = Vector2.Lerp(_moveForce, Vector2.zero, (1-movementConfig.drag));
+            _rb.velocity = Vector2.Lerp(_rb.velocity, Vector2.zero, (1-movementConfig.drag));
+            
+            if (!(Math.Abs(_rb.velocity.magnitude) < 1.5f)) return;
+            _rb.velocity = Vector2.zero;
+            _moveForce = Vector2.zero;
         }
-        
-        
-        private void Accelerate() {
-            // Determine the target velocity based on input
-            var targetVelocity = _moveDirection.normalized * movementConfig.topSpeed;
-            // Calculate the acceleration rate based on the time to reach top speed
-            var acceleration = movementConfig.acceleration / TimeToReachTopSpeed;
-            _rb.velocity = Vector2.MoveTowards(_rb.velocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+
+        private void ApplyAccelerationMovement() {
+            _moveForce += Vector2.Lerp(_ghostForward, _moveDirection, movementConfig.turnRate).normalized * (movementConfig.acceleration * _moveDirection.magnitude);
+            _rb.AddForce(_moveForce, ForceMode2D.Force);
+            _ghostForward = Vector2.Lerp(_ghostForward.normalized, _rb.velocity.normalized, movementConfig.drift).normalized;
         }
 
         public void ForceStop() {
             _rb.velocity = Vector2.zero;
-        }
-        
-        public void ToggleActive() {
-            _isActive = !_isActive;
-        }
-        
-        public void SetActive(bool value) {
-            _isActive = value;
+            _moveForce = Vector2.zero;
         }
 
         private void Dash() {
@@ -93,17 +102,25 @@ namespace PackItUp.Controllers {
             _isDashing = true;
             _timeSinceLastDash = 0;
             _timeDashing = 0;
-            _dashDirection = _rb.velocity.magnitude == 0 ? Vector2.up : _rb.velocity.normalized;
             PerformDash();
         }
         
         private void PerformDash() {
+            
             if (_timeDashing >= movementConfig.dashDuration) {
                 _isDashing = false;
                 return;
             }
-            _rb.velocity = _dashDirection * movementConfig.dashSpeed;
+            _rb.velocity = _ghostForward * (movementConfig.dashMultiplier * movementConfig.topSpeed);
             _timeDashing += Time.fixedDeltaTime;
+        }
+        
+        public void ToggleActive() {
+            _isActive = !_isActive;
+        }
+        
+        public void SetActive(bool value) {
+            _isActive = value;
         }
     }
 }
