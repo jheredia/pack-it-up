830142cf3a553391f73873888a3de97bfa2dd938
Brandon Jeon <62401369+BJeon04@users.noreply.github.com>
2024-06-17 21:03:01 -0400

    Velocity effect stuff
    Some velocity effects stuff

---
+++ Assets/Features/Pickups/Code/Base Classes/Effect_VelocityMod.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_VelocityMod : MonoBehaviour
+{
+    //Fields
+    public float velocityMod;
+
+    //Property
+    public float VelocityMod { get { return velocityMod; } set { velocityMod = value; } }
+}
